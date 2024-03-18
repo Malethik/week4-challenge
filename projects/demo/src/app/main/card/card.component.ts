1ea74da96d8b581e9e60d423d797ccc7293de309
malethik <quintiliani.d@gmail.com>
2024-03-18 21:08:03 +0100

    Updating exercise

--- projects/demo/src/app/main/card/card.component.ts
+++ projects/demo/src/app/main/card/card.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Gentelman } from '../pointer';
+import { gentelmans } from '../pointer.service';
 
 @Component({
   selector: 'isdi-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
-
+  @Input()
+  gentelmans!: Gentelman;
 }
