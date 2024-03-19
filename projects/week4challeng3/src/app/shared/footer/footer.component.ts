c1182fa47ff36055aceb3d0f7a910951d0546f4c
malethik <quintiliani.d@gmail.com>
2024-03-19 19:25:48 +0100

    Initial Commit

---
+++ projects/week4challeng3/src/app/shared/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'w3c2-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+}
---
+++ projects/week4challeng3/src/app/shared/menu/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'w3c2-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+
+}
